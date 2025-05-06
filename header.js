699b7df4f18aa5e56e9a399b8168c94489b8c273
loressm <lorerome90@virgilio.it>
2025-05-06 15:06:19 +0200

    Delete header.js

--- header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('afterbegin', data);
-        })
-        .catch(error => console.error('Errore nel caricamento dell’header:', error));
-});
